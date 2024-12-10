3a6ec6b47e70185c5008e79cc28b460b6890cb30
sxv3 <66145858+sxv3@users.noreply.github.com>
2024-12-09 16:51:06 -0800

    Initial Commit - added header file for node

---
+++ Node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+
+//Node header file.
+//Created by Tai Wong on December 9th
+
+class Node {
+protected:
+  Student* student;
+  Node* node;
+public:
+  Node(Student* student); //constructor
+  Node* getNext();
+  Student* getStudent();
+  void setNext(Node* nextnode);
+  ~Node(); //destructor
+};
+
+#endif
