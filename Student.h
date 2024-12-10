e846b1828f386969d7c6f81ee0f310ff53cd7947
sxv3 <66145858+sxv3@users.noreply.github.com>
2024-12-09 18:34:37 -0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -4,14 +4,20 @@
 #include <iostream>
 #include <cstring>
 
+//Student header file for the student class. 
+//Created by Tai Wong on December 9th
+
 //student class
 class Student {
 private:
+//name of student
   char name[80];
 public:
   //constructor
   Student(const char* name);
+
   const char* getName();
+  void printStudent();
 };
 
 #endif
